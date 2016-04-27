a50320de8116e94ed24074b4de8eafe0a844b941
dayancey <dyancey38@gmail.com>
2016-04-27 09:39:14 -0400

    Initial Commit
    Initial Commit

---
+++ Testing Github/Testing Github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing Github
+//
+//  Created by Devin yancey on 4/27/16.
+//  Copyright © 2016 JK2Designs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Testing Github/Testing Github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Testing Github
+//
+//  Created by Devin yancey on 4/27/16.
+//  Copyright © 2016 JK2Designs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
